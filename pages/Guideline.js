ba88b0303a3d2691b4eaa5143d9a10c690f58b9a
Shivu1912 <shivaniyadav1912@gmail.com>
2025-05-24 15:58:59 +0530

    change all files

---
+++ pages/Guideline.js
@@ -0,0 +1,10 @@
+import { Button, Box } from '@chakra-ui/react';
+
+export default function TestPage() {
+  return (
+    <Box p={4}>
+      <Button colorScheme="teal" color="blue">Guideline</Button>
+    </Box>
+  );
+}
+
